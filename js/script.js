acb4a0b8ac385c918478b754414d576c72ccbdfc
Bibbleboop <nicjah@hotmail.com>
2023-03-15 04:26:52 +0000

    date update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-
+// let today =$("#currentDay");
 // //Todays date in Jan 1st 1999 Format 
 let today = moment();
 $("#currentDay").text(today.format("D MMM YYYY"));
 
 //Day of the week 
-let dayWeek = today.format("[Today is] dddd");
-$("#weekDay").text(dayWeek);
+// let dayWeek = today.format("[Today is] dddd");
+// $("#weekDay").text(dayWeek);
 
 //Formatting the following variables with Moment.js
-function todaysDate() {
-    today = moment().format("MMMM Do YYYY, h:mm:ss a");
-currentDay.text(today);
-$("#displayDateTime").text(now);
-}
+// function todaysDate(today) {
+    today = moment().format("dddd D MMM YYYY, HH:mm:ss a ");
+// currentDay.text(today);
+// $("#displayDateTime").text(now);
+// }
 
